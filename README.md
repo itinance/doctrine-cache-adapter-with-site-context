13a92effd95c8b003a806c5080cf57a4062a7276
Hagen Hübel <hhuebel@itinance.com>
2015-09-13 09:01:48 +0200

    initial

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
-# doctrine-cache-adapter-with-site-context
-CacheAdapter as a bridge between TwigCacheBundle and WhiteLabelBundle 
+# DoctrineCacheAdapterWithSiteContext
+
+If you are using Emanuele Minottos [TwigCacheBundle](https://github.com/EmanueleMinotto/TwigCacheBundle) and
+Simon Ochos white-label-bundle (https://github.com/simonoche/white-label-bundle), you will get trouble with
+the cache.
+
+This class is a replacement for the Cache Adapter used in TwigCacheBundle, to respect the current site context
+in a white-label / multi-site environment.
+
+## Installation:
+
+Installation using composer:
+
+```
+    "repositories": [
+        {
+            "type": "git",
+            "url": "git@github.com:itinance/DoctrineCacheAdapterWithSiteContext.git"
+        }
+    ],
+    "require": {
+        "itinance/DoctrineCacheAdapterWithSiteContext": "dev-master"
+    },
+```
+
+
+## Configuration
+
+in app/config/services.yml add the following
+
+```
+    twig_cache.adapter:
+        class: itinance\DoctrineCacheAdapterWithSiteContext
+        arguments:
+            - @cache_service
+            - @service_container
+```
+
+@cache_service could be configured like this:
+
+```
+    memcached:
+        class: Memcached
+        calls:
+            - [ addServers, [ %memcached.servers% ] ]
+
+    cache_service:
+        class: Doctrine\Common\Cache\MemcachedCache
+        calls:
+            - [ setMemcached, [ @memcached ] ]
+```
+
---
+++ src/DoctrineCacheAdapterWithSiteContext.php
@@ -0,0 +1,74 @@
+<?php
+/*
+ *
+ * (C) ITinance GmbH <https://github.com/itinance/>
+ *
+ * Author: Hagen Huebel, hhuebel@itinance.com
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace itinance;
+
+use Asm89\Twig\CacheExtension\CacheProviderInterface;
+use Asm89\Twig\CacheExtension\CacheProvider\DoctrineCacheAdapter;
+use Doctrine\Common\Cache\Cache;
+use Symfony\Component\DependencyInjection\Container;
+use Symfony\Component\DependencyInjection\ContainerAware;
+
+/**
+ * Class DoctrineCacheAdapterWithSiteContext
+ *
+ * This class builds a bridge between TwigCache-Extension and MultiSite-Extension.
+ *
+ * When TwigCacheExtension will fetch from cache or save to cache, the key will be prefixed
+ * by the current site_context's namepace name.
+ *
+ *
+ * @link https://github.com/EmanueleMinotto/TwigCacheBundle
+ * @link https://github.com/simonoche/white-label-bundle
+ *
+ */
+class DoctrineCacheAdapterWithSiteContext extends DoctrineCacheAdapter
+{
+    /**
+     * @var string
+     */
+    private $contextNamespace;
+
+    /**
+     * @param Cache $cache
+     */
+    public function __construct(Cache $cache, Container $container)
+    {
+        parent::__construct($cache);
+
+        $this->contextNamespace = $container->get('site_context')->getContext()['namespace'];
+    }
+
+    /**
+     * prefix key with current site context's namespace
+     * @param string $key
+     * @return string
+     */
+    private function extendKey($key) {
+        return $this->contextNamespace . '::' . $key;
+    }
+    /**
+     * {@inheritDoc}
+     */
+    public function fetch($key)
+    {
+        return parent::fetch( $this->extendKey( $key) );
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function save($key, $value, $lifetime = 0)
+    {
+        return parent::save( $this->extendKey( $key ), $value, $lifetime);
+    }
+
+}
